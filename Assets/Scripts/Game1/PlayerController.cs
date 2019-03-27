d415ff8bc17259958408a35c502852f115177818
GloriousSindorei <unconfigured@null.spigotmc.org>
2019-03-27 13:37:17 +0100

    GameAdd
    Squidy

---
+++ Assets/Scripts/Game1/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Vector2 usingSpeed;
+    public float movement;
+    private float moving;
+    public float shoot;
+    private bool shooting = false;
+    private Rigidbody2D rigid;
+    Vector3 velocity;
+    public GameObject Bullet;
+    Vector2 bulletPos;
+    public float fireRate = 0.5f;
+    float nextFire = 0.0f;
+
+
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float shoot = Input.GetAxis("Fire1");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        movement = 0;
+        if (shoot > 0 && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            Fire();
+        }
+        if (moveHorizontal > 0)
+        {
+                movement = 1f;
+        }
+        else if (moveHorizontal < 0)
+        {
+            movement = -1f;
+        }
+        Vector3 targetVelocity = new Vector2(movement * speed, 0);
+        rigid.velocity = targetVelocity;
+    }
+    void Fire()
+    {
+        bulletPos = transform.position;
+        Instantiate(Bullet, bulletPos, Quaternion.identity);
+    }
+}
--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -9,7 +9,10 @@ public class LoadLevel : MonoBehaviour
 
     public void LoadScene  ()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Leveltest");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
     }
-
+    public void LoadGame1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game1");
+    }
 }
