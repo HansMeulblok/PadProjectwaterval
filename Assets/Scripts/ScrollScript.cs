8f2ac1ad1906e6d62e8f8a4b4efccd2a0a9b731c
Hans Meulblok <hansmeulblok4@gmail.com>
2019-04-17 21:35:11 +0200

    moving background
    repeat bug ( needs to be fixed)

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    float scrollSpeed = -0.5f;
+    Vector2 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 10);
+        transform.position = startPos + Vector2.down * newPos;
+    }
+}
