c40c817e0cd99bffbb30c802ef37a4170b8a8d02
Hans Meulblok <hansmeulblok4@gmail.com>
2019-04-23 23:21:16 +0200

    Meter output
    pijndisplay, ook enemy animation

--- Assets/Scripts/Rotater22.cs
+++ Assets/Scripts/Rotater22.cs
@@ -1,37 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotater22 : MonoBehaviour
 {
-    private float speed;
-    private float min;
-    private float max;
-    
+    public float speed;
+    public float min;
+    public float max;
 
-    private float z;
+    public float z;
 
-    private void Start()
+    public Text Pijndisplay;
+
+
+    public void Start()
     {
         
         z = transform.localEulerAngles.z;
+        
+    
     }
 
-    private void Update()
+    public virtual void Update()
     {
         min = -180;
         max = 0;
-
+      
         speed = 0.2f;
-
 
 
         var vertical = Input.GetAxis("Mouse Y");
         z -= vertical * speed;
         z = Mathf.Clamp(z, min, max);
+
+
         if (z < max && z > min)
         {
             transform.localEulerAngles = new Vector3(0, 0, z);
+            Debug.Log(transform.localEulerAngles.z);
+
+
+        
         }
+        if (transform.localEulerAngles.z <= 359 && transform.localEulerAngles.z >= 300)
+        {
+            Pijndisplay.transform.rotation = Quaternion.identity;
+            Debug.Log("weinig pijn");
+            Pijndisplay.text = "weinig pijn";
+
+        }
+
+        if (transform.localEulerAngles.z <= 299 && transform.localEulerAngles.z >= 240)
+        {
+            Pijndisplay.transform.rotation = Quaternion.identity;
+            Debug.Log("middelmatige pijn");
+            Pijndisplay.text = "middelmatige pijn";
+        }
+
+        if (transform.localEulerAngles.z <= 239 && transform.localEulerAngles.z >= 180)
+        {
+            Pijndisplay.transform.rotation = Quaternion.identity;
+            Debug.Log("heftige pijn");
+            Pijndisplay.text = "heftige pijn";
+        }
+
+
+
     }
 }
